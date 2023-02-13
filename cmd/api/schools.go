82a04a58c31ba309baea163d340b0d8ef56ed9a8
Dalwin Lewis <lewisdalwin@gmail.com>
2023-02-13 14:40:53 -0600

    get id from URL

--- cmd/api/schools.go
+++ cmd/api/schools.go
@@ -4,6 +4,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) createSchoolHandler(w http.ResponseWriter, r *http.Request) {
@@ -11,5 +14,11 @@ func (app *application) createSchoolHandler(w http.ResponseWriter, r *http.Reque
 }
 
 func (app *application) showSchoolHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "School displayed...")
+	params := httprouter.ParamsFromContext(r.Context())
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "show details of school %d\n", id)
 }
